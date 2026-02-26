ca2fd25544b17e26f121320f52088eaa8d9e457c
gokulakannan3003 <gokul.j@seewise.ai>
2026-02-26 20:39:58 +0530

    Fix flake8 lint rules

--- app/weather_rules.py
+++ app/weather_rules.py
@@ -1,5 +1,4 @@
 import re,os
-from datetime import datetime, timedelta
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 RULES_FILE_PATH = os.path.join(BASE_DIR, "data_bucket", "weather_minima.md")
